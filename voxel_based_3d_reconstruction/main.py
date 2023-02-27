ecf73c304972e5c0e26f4197cf0e950b54c5ba31
FedorTaggenbrock <fedor752@gmail.com>
2023-02-27 16:38:30 +0100

    lookup_table probably works

--- voxel_based_3d_reconstruction/main.py
+++ voxel_based_3d_reconstruction/main.py
@@ -15,17 +15,16 @@ if __name__ == '__main__':
     #cali = Calibration()
     #intrinsics = cali.obtain_intrinsics_from_cameras()
     #cali.obtain_extrinsics_from_cameras()
-
-    S = BackgroundSubstraction()
-    cam_means, cam_std_devs = S.create_background_model()
-    thresholds = np.array([[10, 2, 18],
-                           [10, 2, 14], #very short flicker
-                           [10, 1, 10],
-                           [10, 2, 22]])
-    num_contours = [1, 2, 2, 1]
-    show_video = False
-
-    #masks = S.background_subtraction(thresholds, num_contours, cam_means, cam_std_devs, show_video)
+    # S = BackgroundSubstraction()
+    # cam_means, cam_std_devs = S.create_background_model()
+    # thresholds = np.array([[10, 2, 18],
+    #                        [10, 2, 14], #very short flicker
+    #                        [10, 1, 10],
+    #                        [10, 2, 22]])
+    # num_contours = [1, 2, 2, 1]
+    # show_video = False
+    #
+    # masks = S.background_subtraction(thresholds, num_contours, cam_means, cam_std_devs, show_video)
     masks = load_masks_pickle()
     
     VR = VoxelReconstruction('cameras.pickle')
