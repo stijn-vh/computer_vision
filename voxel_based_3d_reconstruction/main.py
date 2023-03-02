53312af5451bb943a74380a8b29674b46f887d75
FedorTaggenbrock <fedor752@gmail.com>
2023-03-02 12:36:07 +0100

    changed test_vox

--- voxel_based_3d_reconstruction/main.py
+++ voxel_based_3d_reconstruction/main.py
@@ -31,13 +31,13 @@ def determine_new_masks():
     return S.background_subtraction(thresholds, num_contours, cam_means, cam_std_devs, show_video)
 
 if __name__ == '__main__':
-    determine_camera_params()
-    VR = VoxelReconstruction('scaled_camera.pickle')
-    lookup_table = VR.create_lookup_table()
-    pickle_object('lookup_table', lookup_table)
+    #determine_camera_params()
+    VR = VoxelReconstruction('camera.pickle')
+    #lookup_table = VR.create_lookup_table()
+    #pickle_object('lookup_table', lookup_table)
     masks = load_pickle_object('masks')
-    lookup_table = load_pickle_object('lookup_table')
+    #lookup_table = load_pickle_object('lookup_table')
 
-    VR.lookup_table = lookup_table
+    #VR.lookup_table = lookup_table
     VR.test_voxel_reconstruction(masks)
     #VR.run_voxel_reconstruction(masks)
