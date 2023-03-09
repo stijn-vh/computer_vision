4f0047728accd450c1b34542922760e580881a6f
stijn-vh <stijn@zmeel.nl>
2023-03-09 14:18:42 +0100

    add clustering

--- color_based_voxel_labeling/clustering.py
+++ color_based_voxel_labeling/clustering.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 class Clustering:
     criteria = None
@@ -12,9 +13,21 @@ class Clustering:
     
     def cluster(self, voxels):
         # Transform voxels from [x, y, z] to [x, y]
-        data = voxels[:, 0:1]
-        compactness, labels, center = cv.kmeans(data, self.K, None, self.criteria, 10, self.flags)
+        data = np.float32(voxels[:, [0,2]])
+        compactness, labels, centers = cv.kmeans(data, self.K, None, self.criteria, 10, self.flags)
         
-        return 
+        voxel_clusters = []
+
+        for label in enumerate(np.unique(labels)):
+            # Take z where 100 < z < 180 
+            label_idx = np.where(labels == label)[0]
+
+            voxels_with_label = np.take(voxels, label_idx, axis = 0)
+
+            z_idx = np.where(np.logical_and(voxels_with_label[:, 1] > 100, voxels_with_label[:, 1] < 180))
+            voxel_clusters.append(np.take(voxels_with_label, z_idx, axis = 0))
+
+
+        return voxel_clusters, centers, compactness
 
 
