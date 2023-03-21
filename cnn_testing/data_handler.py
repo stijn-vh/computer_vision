5627c71aa7e38f7774ef46931239d40dd3c2648b
stijn-vh <stijn@zmeel.nl>
2023-03-21 12:42:52 +0100

    Update data_handler.py

--- cnn_testing/data_handler.py
+++ cnn_testing/data_handler.py
@@ -1,1 +1,27 @@
-#Imports fashion mnist dataset, split into train and test data, possibly perform preprocessing and augmentation
+import numpy as np
+from tensorflow import keras
+from keras.datasets import fashion_mnist
+from sklearn.model_selection import train_test_split
+
+def grayscale_images(images):
+    return images.astype('float32') / 255
+
+def load_mnist_data():
+    # Load the Fashion MNIST dataset from Keras
+    (X_train_full, Y_train_full), (X_test, Y_test) = fashion_mnist.load_data()
+
+    # Split the full training set into training and validation sets
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X_train_full, Y_train_full, test_size=0.2, random_state=42)
+
+    # Normalize the pixel values to be between 0 and 1
+    X_train = grayscale_images(X_train)
+    X_valid = grayscale_images(X_valid)
+    X_test = grayscale_images(X_test)
+
+    Y_train = grayscale_images(Y_train)
+    Y_valid = grayscale_images(Y_valid)
+    Y_test = grayscale_images(Y_test)
+
+    return X_train, X_valid, X_test, Y_train, Y_valid, Y_test
+
+
